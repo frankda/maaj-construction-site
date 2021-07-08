01bac9c91e1a2adf0672cef7dc21cd70977f9b58
frankda <suyunyi@gmail.com>
2021-07-08 22:07:20 +1000

    - Add animation for card

--- src/components/card-image/card-image.js
+++ src/components/card-image/card-image.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './card-image.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CardImage = () => {
   return (
@@ -7,8 +8,10 @@ const CardImage = () => {
       <div className="card-image__overlay"></div>
       <div className="card-image__content">
         <h4 className="card-image__title">name</h4>
-        <p className="card-image__description"></p>
-        <a className="card-image__cta" href="#">Take a look</a>
+        <div className="card-image__description">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi minus quasi fugiat odit fugit, eos dicta eius optio obcaecati veritatis magnam consequatur, alias, esse in repudiandae tempora animi voluptas corporis!</p>
+        </div>
+        <a className="card-image__cta" href="#">Take a look<FontAwesomeIcon className="card-image__cta-icon" icon="chevron-right" /></a>
       </div>
     </div>
   )
--- src/config/fontawesome.js
+++ src/config/fontawesome.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, faMoon, faSun, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { fas, faMoon, faSun, faPhone, faEnvelope, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fas, faSun, faMoon, faPhone, faEnvelope);
+library.add(fas, faSun, faMoon, faPhone, faEnvelope, faChevronRight);
